8d437e846295886d34b9cc54fbc3146c87a0693e
SC-NSI <59825716+SC-NSI@users.noreply.github.com>
2020-02-22 17:31:06 +0100

    Add files via upload

---
+++ insert.py
@@ -0,0 +1,33 @@
+def swap(t,i,j):
+
+    save=t[i]
+    t[i]=t[j]
+    t[j]=save
+
+    return t
+
+
+
+def insert(t,i):
+
+    for index in range(i,0,-1):
+        if t[index] < t[index-1]:
+            swap(t, index-1, index)
+        else:
+            break
+
+    return t
+
+
+
+def testinsert():
+    t=[3,4,2]
+    i=2
+
+    for index in range(i,0,-1):
+        if t[index] < t[index-1]:
+            swap(t, index-1, index)
+        else:
+            break
+
+    return t

85ac0b5f2f6adee805b0e8a93aa15d1f2a69f4ba
SC-NSI <59825716+SC-NSI@users.noreply.github.com>
2020-02-22 18:19:31 +0100

    Update Remove.py

--- Remove.py
+++ Remove.py
@@ -3,8 +3,8 @@ def remove(t,i):
     resultat=[0]*(len(t)-1)
     for j in range(len(t)):
         if j<i:
-            res[j]=t[j]
+            resultat[j]=t[j]
         if j>i:
-            res[j-1]=t[j]
+            resultat[j-1]=t[j]
 
     return resultat

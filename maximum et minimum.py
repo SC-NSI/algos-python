ba984e2325c74e670568a3de6862a9e23e4dfd8c
SC-NSI <59825716+SC-NSI@users.noreply.github.com>
2020-02-22 17:47:43 +0100

    Add files via upload

---
+++ maximum et minimum.py
@@ -0,0 +1,14 @@
+def maximum(a,b):
+    """a et b sont des nombres de type int etfloat la fonction renvoie le maximum des deux nombres"""
+    if a > b:
+        return a
+    else:
+        return b
+        
+def minimum(a,b):
+    """a et b sont des nombres de type int et float la fonction renvoie le minimum des deux nombres"""
+    if a < b:
+        return a
+    else:
+        return b
+        
